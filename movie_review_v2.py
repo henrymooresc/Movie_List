4e170a2a8e6df4d95ef6bcd8fb9597dcea4a79a1
Henry Moore Work <henrypmoore123@gmail.com>
2021-01-05 13:15:23 -0500

    tkinter semi-try

--- movie_review_v2.py
+++ movie_review_v2.py
@@ -1,38 +1,60 @@
-from tkinter import *
+import tkinter as tkr
+from tkinter import ttk
 import pandas as pd
 
 def submit_fields():
     path = "movielist.xlsx"
     df1 = pd.read_excel(path)
     
-    SeriesTitle = df1["Title"]
-    SeriesDir = df1["Director"]
+    sTitle = df1["Title"]
+    sDir = df1["Director"]
+    #sGenre = df1["Genre(s)"]
+    #sTheme = df1["Theme(s)"]
+    #sRDate = df1["Release"]
+    #sMPAA = df1["MPAA"]
+    #sCast = df1["Cast"]
+    #sRating = df1["Rating"]
+    #sComment = df1["Comments"]
     
-    title = pd.Series(entry1.get())
-    director = pd.Series(entry2.get())
+    title = pd.Series(eTitle.get())
+    director = pd.Series(eDirector.get())
 
-    SeriesTitle = SeriesTitle.append(title)
-    SeriesDir = SeriesDir.append(director)
+    sTitle = sTitle.append(title)
+    sDir = sDir.append(director)
 
-    df2 = pd.DataFrame({"Title":SeriesTitle, "Director":SeriesDir})
+    df2 = pd.DataFrame({"Title":sTitle, "Director":sDir})
     df2.to_excel(path, index=False)
 
-    entry1.delete(0, END)
-    entry2.delete(0, END)
+    eTitle.delete(0, tkr.END)
+    eDirector.delete(0, tkr.END)
 
 
-master = Tk()
+root = tkr.Tk()
+root.geometry("500x500")
+root.title("Film Rating Entry Form")
 
-Label(master, text="Title").grid(row=0)
-Label(master, text="Director").grid(row=1)
+frame = tkr.Frame(root)
+frame.pack()
+topF = tkr.Frame(root)
+topF.pack(side=tkr.TOP)
+botF = tkr.Frame(root)
+botF.pack(side=tkr.BOTTOM)
+leftF = tkr.Frame(root)
+leftF.pack(side=tkr.LEFT)
+rightF = tkr.Frame(root)
+rightF.pack(side=tkr.RIGHT)
 
-entry1 = Entry(master)
-entry2 = Entry(master)
+eTitle = tkr.Entry(leftF, width=20)
+eTitle.insert(0, "Film Title")
+eTitle.pack(padx=3, pady=3)
+eDirector = tkr.Entry(leftF, width=20)
+eDirector.insert(0, "Director Name")
+eDirector.pack(padx=3, pady=3)
+eRelease = tkr.Entry(leftF, width=25)
+eRelease.insert(0, "Release Date (mm/dd/yyyy)")
+eRelease.pack(padx=3, pady=3)
 
-entry1.grid(row=0, column=1)
-entry2.grid(row=1, column=1)
+#tkr.Button(root, text="Quit", command=root.quit).grid(row=5, column=0, pady=4)
+#tkr.Button(root, text="Submit", command=submit_fields).grid(row=5, column=1, pady=4)
 
-Button(master, text="Quit", command=master.quit).grid(row=3, column=0, pady=4)
-Button(master, text="Submit", command=submit_fields).grid(row=3, column=1, pady=4)
-
-mainloop()
+tkr.mainloop()
